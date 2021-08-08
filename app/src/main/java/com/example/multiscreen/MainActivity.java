70f6b230915ef847862f1f9747d71f7e30c77d89
SakshamGurbhele <saksham9013gurbhele@gmail.com>
2021-08-08 23:54:10 +0530

    Added features

--- app/src/main/java/com/example/multiscreen/MainActivity.java
+++ app/src/main/java/com/example/multiscreen/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.multiscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    private Button button;
+    private TextView EditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = R.id.button;
+        button =findViewById(R.id.button);
+        EditText = findViewById(R.id.EditText);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String s = EditText.getText().toString();
+                Toast.makeText(MainActivity.this, "This "+ s, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
